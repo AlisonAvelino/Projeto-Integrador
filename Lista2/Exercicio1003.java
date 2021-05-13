c31d278ec37e40298c27ffea0dd50022edb3c20c
Alison Avelino <alisonavelino3133@gmail.com>
2021-05-12 22:55:44 -0300

    Exercicio 1003 - Exercicio de soma simples

---
+++ Lista2/Exercicio1003.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio1003 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int A, B, SOMA;
+        
+        A = leitor.nextInt();
+        B = leitor.nextInt();
+
+        SOMA = A+B;
+        
+        System.out.println("SOMA = " + SOMA);
+    }
+    
+}
